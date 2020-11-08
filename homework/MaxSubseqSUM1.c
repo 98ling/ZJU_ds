e45129e3c4b5379420bc1d6fb0b4f8c0350a42e0
panghu <943262093@qq.com>
2020-11-08 20:54:02 +0800

    1.1-subseqsum1

---
+++ homework/MaxSubseqSUM1.c
@@ -0,0 +1,14 @@
+int MaxSubseqSum1(int List[],int,int N){
+    int thissum,maxsum = 0;
+    int i,j,k;
+    for(i=0;i<N;i++){
+        thissum = 0;
+	for(k=i;l<N;k++){
+            thissum += List[K];
+	}
+	if(thissum>maxsum){
+	    maxsum = thissum; 
+	}
+    }
+    return maxsum;
+}
